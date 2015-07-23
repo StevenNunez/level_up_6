71b4775d7d3c699c79c4d07aafb29d050cb26f2c
Steven Nunez <steven.nunez@gmail.com>
2015-07-22 20:47:07 -0400

    Oh so functional

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,24 @@
 
   app.controller('StoreController', function(){
     this.products = shirts;
-  })
+  });
+
+  app.controller('PanelController', function(){
+    this.tab = 1;
+    this.setTab = function(newTab){
+      this.tab = newTab;
+    };
+
+    this.isCurrentTab = function(tab){
+      return this.tab === tab;
+    };
+
+    this.activeClass = function(tab){
+      if(this.tab === tab){
+        return "banana";
+      }
+    };
+  });
 
   var shirts = [
       { shirtName: 'Casual Friday',
